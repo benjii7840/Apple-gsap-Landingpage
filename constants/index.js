1fe791a44dda785d3ff83e440611daec3cb3d607
Benjamin Baraza <barazabenjamin58@gmail.com>
2025-12-12 10:51:20 +0300

    Initial commit

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+            {label: 'store' },
+            {label: 'Mac' },
+            {label: 'Iphone' },
+            {label: 'Watch' },
+            {label: 'Vision' },
+            {label: 'Airpods' },
+
+          ] 
